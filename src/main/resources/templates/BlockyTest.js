de8b83bd45d3eb7049905392a49d97110405f305
radu061 <raduciocan1611@gmail.com>
2026-07-09 10:54:43 +0300

    Test Blocky

--- src/main/resources/templates/BlockyTest.js
+++ src/main/resources/templates/BlockyTest.js
@@ -1,9 +1,19 @@
+const toolbox = {
+  // There are two kinds of toolboxes. The simpler one is a flyout toolbox.
+  kind: 'flyoutToolbox',
+  // The contents is the blocks and other items that exist in your toolbox.
+  contents: [
+    {
+      kind: 'block',
+      type: 'controls_if',
+    },
+    {
+      kind: 'block',
+      type: 'controls_whileUntil',
+    },
+    // You can add more blocks to this array.
+  ],
+};
 
-
-const workspace = Blockly.inject('blocklyDiv', 
-{
-});
-
-const workspace = Blockly.inject(document.getElementById('blocklyDiv'), 
-{
-});
+// The toolbox gets passed to the configuration options during injection.
+const workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox });
