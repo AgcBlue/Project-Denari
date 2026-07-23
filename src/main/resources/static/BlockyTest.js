3634aac305a3fd7d820786dfc955786e0f04c851
agcblue <alexchirvasie@gmail.com>
2026-07-23 08:35:31 +0300

    test

--- src/main/resources/static/BlockyTest.js
+++ src/main/resources/static/BlockyTest.js
@@ -1,23 +1,63 @@
-const toolbox = {
-  // There are two kinds of toolboxes. The simpler one is a flyout toolbox.
-  kind: 'flyoutToolbox',
-  // The contents is the blocks and other items that exist in your toolbox.
-  contents: [
+const myTheme = Blockly.Theme.defineTheme('myTheme', 
+{
+  'base': Blockly.Themes.Classic,
+  'blockStyles': 
+  {
+    'costumControlBlock': 
     {
-      kind: 'block',
-      type: 'controls_if',
-    },
-    {
-      kind: 'block',
-      type: 'controls_whileUntil',
-    },
-    {
-    kind: 'block',
-    type:'controls_for',
+      'colourPrimary': '#66baff',
+      'colourSecondary': '#43a9fd',
+      'colourTertiary': '#1897ff'
     }
-    // You can add more blocks to this array.
+  },
+  'componentStyles': {},
+  'blockTypeStyleMap':
+  {
+    'controls_if': 'costumControlBlock',   // aplică noul style DOAR pe controls_if
+    'controls_whileUntil': 'costumControlBlock'   // aplică noul style DOAR pe controls_if
+  }
+});
+
+
+const toolbox = 
+{
+  kind: 'categoryToolbox',
+  contents: 
+  [
+    {
+      kind: 'category',
+      name: 'Control',
+      colour: '#0590e0',
+      contents: 
+      [
+        {
+          kind: 'block',
+          type: 'controls_if',
+        },
+        {
+          kind: 'block',
+          type: 'controls_whileUntil',
+        },
+        {
+          kind: 'block',
+          type: 'controls_for',
+        }
+      ],
+    },
+
+    {
+      kind: 'category',
+      name: 'Logic',
+      contents:
+      [
+        {
+          kind: 'block',
+          type: 'logic_compare'
+        }
+      ]
+    }
   ],
 };
 
 // The toolbox gets passed to the configuration options during injection.
-const workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox });
+const workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox, theme: myTheme });
